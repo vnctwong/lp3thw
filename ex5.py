6db91cd86bc39ed8d17ef1d1af18d2495ab74767
VWMAC <vwmac@VWMACs-Air.vc.shawcable.net>
2019-05-07 15:54:13 -0700

    ex5

---
+++ ex5.py
@@ -0,0 +1,14 @@
+# quotes for string
+# {var} for embedded variables
+# f'' for formatting?
+
+my_name = "Zed Shaw"
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = "blue"
+
+total = my_age + my_height + my_weight
+
+print(f"lets talk about {my_name}.")
+print(f"if I add {my_age}, {my_height}, and {my_weight} i get {total}.")
