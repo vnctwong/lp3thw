c5eadd8f7f9e1a676b13579d814e53e5d0bd679f
VWMAC <vwmac@VWMACs-Air.vc.shawcable.net>
2019-05-07 16:29:48 -0700

    ex6

---
+++ ex6.py
@@ -0,0 +1,16 @@
+types_of_people = 10
+x = f"there are {types_of_people} types_of_people"
+
+binary = "binary"
+do_not = "dodnt"
+y = f"those who know {binary} and those who {do_not}"
+
+print(x)
+print(y)
+
+print(f"Isaid: {x}")
+
+hilarious = False
+jokeEval = "funny y/n?! {}"
+
+print(jokeEval.format(hilarious))
