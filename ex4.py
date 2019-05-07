d03456a3b10b747a13512b8cdbedae1637bd5595
VWMAC <vwmac@VWMACs-Air.vc.shawcable.net>
2019-05-07 15:26:17 -0700

    skipped ex3

---
+++ ex4.py
@@ -0,0 +1,13 @@
+# no let/const
+cars = 100
+drivers = 30
+
+# snake case y
+cars_not_driven = cars - drivers
+
+print("there are", cars, "cars here")
+print("there are", drivers, "drivers here")
+print("there are", cars_not_driven, "cars not driven")
+
+#
+
