efd44310f925fcc1ddea7edaee41432e8a9c9fbe
VWMAC <vwmac@VWMACs-Air.vc.shawcable.net>
2019-05-07 17:37:48 -0700

    done

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("how old are you?", end=" ")
+age = input()
+print("How tall are you?", end=" ")
+height = input()
+print("How much do you weigh?", end=" ")
+weight = input()
+
+print(f"you're {age} old, {height} tall and {weight} heavy")
+
+# input from terminal all str.
+# can use int = int(input())
