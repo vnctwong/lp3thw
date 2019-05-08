3f1322cc7ffb932d09397a8002f9a252bff749e5
VWMAC <vwmac@VWMACs-Air.vc.shawcable.net>
2019-05-07 18:05:53 -0700

    .

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second = argv
+
+input1 = input("type hello. ")
+input2 = input("type world. ")
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+
