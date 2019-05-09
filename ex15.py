dd759e74cd7407b4925b9b318ba39dd717d1a96b
VWMAC <vwmac@VWMACs-MacBook-Air.local>
2019-05-08 21:43:10 -0700

    uses .read to open uri w/ terminal argv

---
+++ ex15.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+script, filename = argv
+# txt is a variable
+txt = open(filename)
+
+print(f" Here's your file {filename}:\n")
+print(txt.read())
+
+print("type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
