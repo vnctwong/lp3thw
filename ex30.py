b17741cf95e6a2a80fb5ffc0c357c80cfbb514fc
VWMAC <vincentkywong@gmail.com>
2019-05-28 23:57:53 -0700

    else if

---
+++ ex30.py
@@ -0,0 +1,24 @@
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print("cars > ppl")
+elif cars < people:
+    print("cars < ppl")
+else:
+    print("no decide")
+
+
+if trucks > cars:
+    print("trucks > cars")
+elif trucks < cars:
+    print("trucks > cars")
+else:
+    print("no decide")
+
+
+if people > trucks:
+    print("ppl > trucks")
+else:
+    print("stay home")
