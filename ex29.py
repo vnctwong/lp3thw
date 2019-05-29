e4f3941633e8eeb4380b62b530faa87ab6417356
VWMAC <vincentkywong@gmail.com>
2019-05-28 23:31:49 -0700

    if statements

---
+++ ex29.py
@@ -0,0 +1,26 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print("more cats")
+
+if people > cats:
+    print("more ppl")
+
+if people < dogs:
+    print("more dogs")
+
+if people > dogs:
+    print("more ppl")
+
+dogs += 5
+
+if people >= dogs:
+    print("ppl >= dogs")
+
+if people <= dogs:
+    print("ppl <= dogs")
+
+if people == dogs:
+    print("ppl are dogs")
