8bc190fed559ac4d0f088faf9490bcce067e355c
VWMAC <vincentkywong@gmail.com>
2019-05-30 03:34:16 -0700

    while loops using function to call

--- ex33.py
+++ ex33.py
@@ -1,17 +1,16 @@
-i = 0
-numbers = []
+def while_loop(num):
+    i = 0
+    numbers = []
 
-while i < 6:
-    print(f"at the top i is {i}")
-    numbers.append(i)
+    while i < num:
+        print(f"at the top i is {i}")
+        numbers.append(i)
 
-    i = i + 1
-    print("numbers now:", numbers)
-    print(f"at the bottom i is {i}")
+        i = i + 1
+        print("numbers now:", numbers)
+        print(f"at the bottom i is {i}")
 
 
+while_loop(6)
 print("the numbers: ")
-
-for num in numbers:
-    print(num)
 
